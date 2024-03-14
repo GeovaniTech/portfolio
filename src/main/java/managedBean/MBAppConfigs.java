7981117fc385d74c45adc36e6f0d8cd6635615b1
GeovaniTech <geovanidebastiani32@gmail.com>
2024-03-14 19:46:02 -0300

    refactor: Separating sections in different files and conveting to use EN or PT

--- src/main/java/managedBean/MBAppConfigs.java
+++ src/main/java/managedBean/MBAppConfigs.java
@@ -1,12 +1,103 @@
 package managedBean;
 
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.context.ExternalContext;
+import jakarta.faces.context.FacesContext;
 import jakarta.inject.Named;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import util.CookieUtil;
 
 @Named(MBAppConfigs.MANAGED_BEAN_NAME)
 @SessionScoped
-public class MBAppConfigs {
+public class MBAppConfigs implements Serializable {
+	private static final long serialVersionUID = 3526359354412555046L;
+
 	public static final String MANAGED_BEAN_NAME = "MBAppConfigs";
+	
+	private String language;
+	private boolean darkMode;
+	
+	private List<Locale> localeList;
+	
+	public MBAppConfigs() {
+		this.setLocaleList(new ArrayList<Locale>());
+		this.getLocaleList().add(new Locale("pt_BR"));
+		this.getLocaleList().add(new Locale("en_US"));
+		
+		
+		//Initial Configurations
+		this.setLanguage(Locale.getDefault().getLanguage());
+		this.setDarkMode(false);
+		
+		this.getConfigsFromCookies();
+	}
+	
+ 	public void updateUserConfigs() { 		
+ 		this.createCookiePreferences();
+ 	}
+	
+ 	public boolean getConfigsFromCookies() {
+ 		this.setDarkMode(CookieUtil.getDarkModeCookie());
+ 		
+ 		if(CookieUtil.getLanguageCookie() != null) {
+ 			this.setLanguage(CookieUtil.getLanguageCookie());
+ 			
+ 			return true;
+ 		}
+ 		return false;
+ 	}
+ 	
+	public void refreshPage() throws IOException {
+		try {
+			ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+			ec.redirect("http://localhost:8080/portfolio");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+ 	
+	public void createCookiePreferences() {	
+		HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+		
+		Cookie darkMode = new Cookie("darkMode", "" + this.isDarkMode());
+		darkMode.setMaxAge(60*60*24*30);
+		darkMode.setPath("/portfolio");
+		
+		Cookie language = new Cookie("language", this.getLanguage());
+		language.setMaxAge(60*60*24*30);
+		language.setPath("/portfolio");
+		
+		response.addCookie(darkMode);
+		response.addCookie(language);
+	}
+	
+	// Getters and Setters
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	public boolean isDarkMode() {
+		return darkMode;
+	}
+	public void setDarkMode(boolean darkMode) {
+		this.darkMode = darkMode;
+	}
+	public List<Locale> getLocaleList() {
+		return localeList;
+	}
+	public void setLocaleList(List<Locale> localeList) {
+		this.localeList = localeList;
+	}
 	
 	
 }
---
+++ src/main/java/util/CookieUtil.java
@@ -0,0 +1,38 @@
+package util;
+
+import jakarta.faces.context.FacesContext;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+
+public class CookieUtil {
+	public static String getLanguageCookie() {
+		HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext()
+				.getRequest();
+		Cookie[] cookies = request.getCookies();
+
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if (cookie.getName().equals("language")) {
+					return cookie.getValue();
+				}
+			}
+		}
+
+		return null;
+	}
+
+	public static boolean getDarkModeCookie() {
+		HttpServletRequest request = (HttpServletRequest) FacesContext.getCurrentInstance().getExternalContext()
+				.getRequest();
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if (cookie.getName().equals("darkMode")) {
+					return Boolean.parseBoolean(cookie.getValue());
+				}
+			}
+		}
+
+		return false;
+	}
+}
